221f2f46c5140b9bc44ae75c77a203738f972551
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-31 11:59:25 -0800

    added state to track in app.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,14 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
-  // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [cityName, setCityName] = useState('Portland');
+  // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for  example
+  const [slogans, setSlogans] = useState(['The City of Excellence']);
+
 
   return (
     <div className="App">
