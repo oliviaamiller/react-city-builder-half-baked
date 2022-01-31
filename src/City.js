93cafaaaeed34202f4e31216b02ab175bcb8d214
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-31 12:27:10 -0800

    added image tags and passed props to city component

--- src/City.js
+++ src/City.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-export default function City(props) {
+export default function City({ waterfrontId, skylineId, castleId }) {
   return (
     <div className="city">
-      {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
+      {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
       {/* use these 3 ids in img tags load approriate images that live in the public directory */}
+      <img src = {`./public/waterfront-${waterfrontId}.jpeg`} />
+      <img src = {`./public/skyline-${skylineId}.jpeg`} />
+      <img src = {`./public/castle-${castleId}.jpeg`} />
     </div>
   );
 }
