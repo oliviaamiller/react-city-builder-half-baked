6cecdb3ae18677193a344f1a545daf985eb3801a
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-31 12:36:17 -0800

    images rendering correctly

--- src/City.js
+++ src/City.js
@@ -5,9 +5,9 @@ export default function City({ waterfrontId, skylineId, castleId }) {
     <div className="city">
       {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
       {/* use these 3 ids in img tags load approriate images that live in the public directory */}
-      <img src = {`./public/waterfront-${waterfrontId}.jpeg`} />
-      <img src = {`./public/skyline-${skylineId}.jpeg`} />
-      <img src = {`./public/castle-${castleId}.jpeg`} />
+      <img src = {`waterfront-${waterfrontId}.jpeg`} />
+      <img src = {`skyline-${skylineId}.jpeg`} />
+      <img src = {`castle-${castleId}.jpeg`} />
     </div>
   );
 }
